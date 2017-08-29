be5ed5f732e410017bf50beb11d8750bdbe0374c
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2017-08-29 17:33:13 +0100

    my first crud app

---
+++ books.js
@@ -0,0 +1,30 @@
+db.books.insert([{
+  title: 'Refactoring the DOM',
+  author: 'Joe Blow',
+  category: 'Technology'
+}, {
+  title: 'Learn Colloquial Speech',
+  author: 'Susie Q',
+  category: 'Humanities'
+}, {
+  title: 'Study of the Brain',
+  author: 'Matt G',
+  category: 'Health'
+}])
+
+
+db.books.insert({
+  title: 'Refactoring the DOM',
+  author: 'Joe Blow',
+  category: 'Technology'
+})
+db.books.insert({
+  title: 'Learn Colloquial Speech',
+  author: 'Susie Q',
+  category: 'Humanities'
+})
+db.books.insert({
+  title: 'Study of the Brain',
+  author: 'Matt G',
+  category: 'Health'
+})
